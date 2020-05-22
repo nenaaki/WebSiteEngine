ba5bec6ea4a379cc84512fce85fbb72a62b609b9
nenaaki <nenaaki@gmail.com>
2020-05-22 12:19:15 +0900

    SignalRのサンプルに沿って作ったページ

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using WebSiteEngine.Shared;
+
+namespace WebSiteEngine.Server
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync(ApplicationValues.HubReceiverName, user, message);
+        }
+    }
+}
---
+++ Shared/ApplicationValues.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSiteEngine.Shared
+{
+    public static class ApplicationValues
+    {
+        public const string HubReceiverName = "ReceiveMessage";
+    }
+}
