e9a59991217af26b81898c7a18f056db02c6e4ef
nenaaki <nenaaki@gmail.com>
2020-05-27 20:47:42 +0900

    キャンバス関連を分離 IndexにTextileのひな型を実装 サーバーにTextileのAPIを追加(暫定)

---
+++ Server/Controllers/TextileDocumentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebSiteEngine.Shared;
+
+namespace WebSiteEngine.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextileDocumentController : ControllerBase
+    {
+        [HttpGet("{localPath}")]
+        public IActionResult Get(string localPath)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "local", localPath);
+
+            return Ok(new TextileDocument { Body = System.IO.File.ReadAllText(path) });
+        }
+    }
+}
---
+++ Shared/TextileDocument.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebSiteEngine.Shared
+{
+    public class TextileDocument
+    {
+        public static class Tags
+        {
+            public const string H1 = "# ";
+
+            public const string H2 = "## ";
+
+            public const string H3 = "### ";
+        }
+
+        /// <summary>
+        /// Textile本体
+        /// </summary>
+        public string Body { get; set; }
+    }
+}
